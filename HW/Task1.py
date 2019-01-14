0b1c5ea2ee4cce9d86f1e38eb94cd72a405dfe4d
volmaletc <vol.maletc@gmail.com>
2019-01-14 13:37:43 +0200

    add first HM

---
+++ HW/Task1.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+place = input("Where are you live? ")
+
+print("Hello, " + name)
+print ("Your age, " + age)
+print ("You live in, " + place)
---
+++ HW/Task2.py
@@ -0,0 +1,11 @@
+a1 = int(input("Enter first number a: "))
+b2 = int(input("Enter second number b: "))
+
+c = a1 + b2
+d = a1 - b2
+c = str(c)
+d = str(d)
+
+print("a + b = " + c)
+print("a - b = " + d)
+
